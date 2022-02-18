e713c127cebb7fbf44b32017a4a0791a5d40e8b4
JavierS084 <javier.sanchezb084@gmail.com>
2022-02-18 10:59:11 -0300

    V0.3

--- src/routes/employees.js
+++ src/routes/employees.js
@@ -26,5 +26,49 @@ router.get('/:id', (req, res) =>{
 });
 
 
-router .post()
+router .post('/', (req, res) => {
+    const { id, name, salary } = req.body;
+    const query = `
+        
+        CALL employeeAddOrEdit(?, ?, ?);
+
+    `;
+    mysqlConnection.query(query, [id, name, salary], (err, rows, fields) => {
+        if (!err) {
+            res.json({Status: 'Employeed Saved'});
+
+        }else{
+            console.log(err);
+        }
+    });
+});
+
+
+router.put('/:id', (req, res) => {
+    const  {name, salary } = req.body;
+    const {id} = req.params;
+    const query = 'CALL employeeAddOrEdit(?,?,?)';
+    mysqlConnection.query(query, [id, name, salary], (err, rows, fields) => {
+        if (!err) {
+            res.json({staus: 'Employeed Update'});
+
+        }else {
+            console.log(err);
+        }
+    })
+});
+
+
+
+router.delete('/:id', (req, res) => {
+    const { id} = req.params;
+    mysqlConnection.query('DELETE FROM employees WHERE id = ?', [id], (err, rows, fields) => {
+        if (!err) {
+            res.json({status: 'Employeed Deleted'});
+        }else{
+            console.log(err);
+        }
+    });
+});
+
 module.exports = router;
